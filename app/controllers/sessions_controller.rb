f4efd3bb9d1a6c77ef751061f00e6d1ae229e05f
Diako999 <diakoilbagy@gmail.com>
2021-09-01 11:35:45 -0400

    login and logout resource

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
   def new
-    
+  end
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "logged in was successful"
+      redirect_to root_path
+    else
+      flash.now[:error] = "username or password was incorrect"
+      render 'new'
+    end
+  end
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "logged out successfully"
+    redirect_to login_path
   end
 end
